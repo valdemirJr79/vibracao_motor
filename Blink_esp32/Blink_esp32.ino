ccfd5eb4ed5dc592d702716ef188bdbf786974c6
GIANLUCA GRIGNANI <gianluca_grignani@estudante.sesisenai.org.br>
2025-07-31 20:35:38 -0300

    up

--- Blink_esp32/Blink_esp32.ino
+++ Blink_esp32/Blink_esp32.ino
@@ -2,3 +2,15 @@
 //O código deve definir o pino do LED como saída e, em seguida, alterna entre os estados HIGH (ligado) e LOW (desligado) 
 //com um atraso de tempo. 
 
+#define LED_BUILTIN 2
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
