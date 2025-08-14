9f582b9b7c0c20993563bd029cd82ffa0bc6eba8
e-valdemirsj@weg.net <e-valdemirsj@weg.net>
2025-08-13 23:37:27 -0300

    subindo arquivos de códigos

---
+++ Bibliotecas/MQTTPubSubClient/examples/EtherMQTT/EtherMQTT.ino
@@ -0,0 +1,68 @@
+#include <Ethernet.h>
+#include <MQTTPubSubClient.h>
+
+uint8_t mac[] = {0xAB, 0xCD, 0xEF, 0x01, 0x23, 0x45};
+const IPAddress ip(192, 168, 0, 201);
+
+EthernetClient client;
+MQTTPubSubClient mqtt;
+
+void connect() {
+connect_to_host:
+    Serial.print("connecting to host...");
+    client.stop();
+    while (!client.connect("public.cloud.shiftr.io", 1883)) {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(" connected!");
+
+    Serial.print("connecting to mqtt broker...");
+    mqtt.disconnect();
+    while (!mqtt.connect("arduino", "public", "public")) {
+        Serial.print(".");
+        delay(1000);
+        if (client.connected() != 1) {
+            Serial.println("WiFiClient disconnected");
+            goto connect_to_host;
+        }
+    }
+    Serial.println(" connected!");
+}
+
+void setup() {
+    Serial.begin(115200);
+    Ethernet.begin(mac, ip);
+
+    // initialize mqtt client
+    mqtt.begin(client);
+
+    // connect to host and mqtt broker
+    connect();
+
+    // subscribe callback which is called when every packet has come
+    mqtt.subscribe([](const String& topic, const String& payload, const size_t size) {
+        Serial.println("mqtt received: " + topic + " - " + payload);
+    });
+
+    // subscribe topic and callback which is called when /hello has come
+    mqtt.subscribe("/hello", [](const String& payload, const size_t size) {
+        Serial.print("/hello ");
+        Serial.println(payload);
+    });
+}
+
+void loop() {
+    mqtt.update();  // should be called
+
+    if (!mqtt.isConnected()) {
+        connect();
+    }
+
+    // publish message
+    static uint32_t prev_ms = millis();
+    if (millis() > prev_ms + 1000) {
+        prev_ms = millis();
+        mqtt.publish("/hello", "world");
+    }
+}
