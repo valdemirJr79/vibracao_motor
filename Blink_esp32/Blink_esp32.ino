0ed7b9fc0e12ab06728846cff91fac8234645811
Jair Lopes <antonio_j_jesus@estudante.sc.senai.br>
2025-08-19 21:30:38 -0300

    mesclagem do codigo OTA com LED

--- Blink_esp32/Blink_esp32.ino
+++ Blink_esp32/Blink_esp32.ino
@@ -1,5 +1,71 @@
-//Escrever um código para fazer um LED piscar em um ESP32 usando a IDE do Arduino. 
-//O código deve definir o pino do LED como saída e, em seguida, alterna entre os estados HIGH (ligado) e LOW (desligado) 
-//com um atraso de tempo. 
+#include <WiFi.h>
+#include <ArduinoOTA.h>
+#define LED 2
+const char *ssid = "REDEWORK";
+const char *password = "Acessonet05";
 
-teste
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  digitalWrite(LED, HIGH); // Liga o LED
+  Serial.println("LED Aceso");
+  delay(500); // Aguarda 1 segundo
+
+  digitalWrite(LED, LOW); // Desliga o LED
+  Serial.println("LED Apagado");
+  delay(500); // Aguarda 1 segundo
+
+}
