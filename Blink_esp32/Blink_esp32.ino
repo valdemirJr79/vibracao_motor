994e0642535d552db4ef4751c69a7041f27fb135
GIANLUCA GRIGNANI <gianluca_grignani@estudante.sesisenai.org.br>
2025-08-21 18:59:47 -0300

    primeiro commit

--- Blink_esp32/Blink_esp32.ino
+++ Blink_esp32/Blink_esp32.ino
@@ -1,6 +1,6 @@
 //Escrever um código para fazer um LED piscar em um ESP32 usando a IDE do Arduino. 
 //O código deve definir o pino do LED como saída e, em seguida, alterna entre os estados HIGH (ligado) e LOW (desligado) 
-//com um atraso de tempo. 
+//com um atraso de tempo ll. 
 
 #define LED_BUILTIN 2
 
