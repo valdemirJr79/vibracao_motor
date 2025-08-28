60ed4dde04c0313eefc41f0be8f7f8b028c8a1a4
matheusschuldt <matheus_schuldt@estudante.sesisenai.org.br>
2025-08-28 20:51:44 -0300

    Trabalho Sistema IoT para Monitoramento e Redução de Desperdício de Água em Jardins realizado 28/08/2025, Matheus Henrique, Lucas e Cauã

--- sketch_jul31a/sketch_jul31a.ino
+++ sketch_jul31a/sketch_jul31a.ino
@@ -1,14 +1,62 @@
-#define LED_BUILTIN 2 
+#include <WiFi.h>
+#include <ArduinoOTA.h>
 
-void setup() { 
- 
-  pinMode(LED_BUILTIN, OUTPUT); 
+const char *ssid = "REDEWORK";
+const char *password = "Acessonet05";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting...");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  // Aguarda conexão WiFi
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Configura OTA
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() { 
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(50); 
-  digitalWrite(LED_BUILTIN, LOW); 
-  
-  delay(50); 
+void loop() {
+  ArduinoOTA.handle();
 }
